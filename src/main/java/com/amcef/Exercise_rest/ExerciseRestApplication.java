c311b8da5c0ef342937161a976291ab7d101f8c8
ivo <ivo2501@gmail.com>
2024-09-05 19:48:07 +0200

    Initial project commit

---
+++ src/main/java/com/amcef/Exercise_rest/ExerciseRestApplication.java
@@ -0,0 +1,13 @@
+package com.amcef.Exercise_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amcef/Exercise_rest/ExerciseRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amcef.Exercise_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
